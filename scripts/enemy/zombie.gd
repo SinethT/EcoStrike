3b998469bec5aef0b4201a1148548f4e8e98d099
SinethT <103297183+SinethT@users.noreply.github.com>
2025-07-14 02:01:47 +1200

    added zombie hit reaction

--- scripts/enemy/zombie.gd
+++ scripts/enemy/zombie.gd
@@ -4,6 +4,13 @@ extends CharacterBody3D
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
+@onready var animation_player = $zombie/AnimationPlayer
 
 func _ready():
-	$zombie/AnimationPlayer.play("Armature|Idle")
+	animation_player.play("Armature|Idle")
+	# Add zombie to Target group so it can be hit by bullets
+	add_to_group("Target")
+
+# Called by bullets/projectiles when they hit the zombie
+func Hit_Successful(_damage: float, _direction: Vector3 = Vector3.ZERO, _hit_position: Vector3 = Vector3.ZERO):
+		animation_player.play("Armature|Hit_reaction")
